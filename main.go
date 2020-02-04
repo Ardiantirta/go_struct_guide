0e2235c55e287e1ed2ba75fd2b1aef5c7481358a
Ardian Tirta <ardiantirtaa@gmail.com>
2020-02-04 09:28:41 +0700

    commit

--- main.go
+++ main.go
@@ -2,13 +2,30 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	a := person{firstName: "alex", lastName: "brown"}
 	fmt.Println(a)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Carry",
+		contact: contactInfo{
+			email:   "jim.carry@gmail.com",
+			zipCode: 94122,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 
 }
